ebd2fdadc1f57f15521e0ffd600bf44367027594
nabi06 <125720183+nabi06@users.noreply.github.com>
2024-02-03 19:23:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("hi")
+
+function handleclick(){
+    let sub=document.getElementById("sub")
+    console.log("clicked");
+    sub.style.filter = "none";
+    sub.style.pointerEvents = "all";
+    
+
+}
+function handlesubmit(){
+    console.log("hi")
+}
